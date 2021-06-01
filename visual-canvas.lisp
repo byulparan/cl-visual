9f5f79d7677cd125e4e368e5816284e3bddffc1f
byulparan <byulparan@gmail.com>
2021-06-01 15:40:04 +0900

    renaming gfx::*gfx-uniform-table*

--- visual-canvas.lisp
+++ visual-canvas.lisp
@@ -1,7 +1,7 @@
 (in-package :cl-visual)
 
 (defun gfx::add-uniform (name type)
-  (setf (gethash name gfx::*gfx-secondary-variables*) (glsl::make-code-object type (ppcre:regex-replace-all "-" (string-downcase name) "_"))))
+  (setf (gethash name gfx::*gfx-uniform-table*) (glsl::make-code-object type (ppcre:regex-replace-all "-" (string-downcase name) "_"))))
 
 (defun gfx::clear-pipeline (&optional remove-uniforms)
   (gfx:reinit-shader-system)
