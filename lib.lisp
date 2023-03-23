9bdb16a1b1d64e351fee2a5cbaaa7b29c7b0cbdd
Parksungmin <byulparan@gmail.com>
2023-03-23 17:19:03 +0900

    add `with-twigl`

--- lib.lisp
+++ lib.lisp
@@ -186,9 +186,6 @@
 (define-function-library flip ((uv :vec2))
   (v! (x uv) (- 1.0 (y uv))))
 
-(define-macro-library get-data (texture indx)
-  `(x (texture ,texture (v! ,indx .0))))
-
 (define-function-library syphon-ratio ((uvn :vec2) (iresolution :vec2) (width :float) (height :float) (x-ratio :float) (h-ratio :float))
   (let* ((w (* (x iresolution) x-ratio (/ height (y iresolution))))
 	 (h (* height (+ (y uvn) (* (/ .5 h-ratio) (- 1.0 h-ratio))) h-ratio)))
@@ -319,6 +316,266 @@
 			      voxel-pad)))
        (setf nearest-voxel (max voxel-pad nearest-voxel)
 	     ,dist (min ,dist nearest-voxel)))))
+
+
+
+
+
+;; ================================================================================
+;; twiGL
+;; ================================================================================
+
+(define-function-library mod289-fl ((x :float))
+  (- x (* (floor (* x (/ 1.0 289.0))) 289.0)))
+
+(define-function-library mod289-v2 ((x :vec2))
+  (- x (* (floor (* x (/ 1.0 289.0))) 289.0)))
+
+(define-function-library mod289-v3 ((x :vec3))
+  (- x (* (floor (* x (/ 1.0 289.0))) 289.0)))
+
+(define-function-library mod289-v4 ((x :vec4))
+  (- x (* (floor (* x (/ 1.0 289.0))) 289.0)))
+
+(define-macro-library mod289 (x)
+  (let* ((type (glsl::code-type (glsl::compile-form  x))))
+    (ecase type
+      (:int `(mod289-fl (* 1.0 ,x)))
+      (:float `(mod289-fl ,x))
+      (:vec2  `(mod289-v2 ,x))
+      (:vec3  `(mod289-v3 ,x))
+      (:vec4  `(mod289-v4 ,x)))))
+
+(unexport '(mod289-fl mod289-v2 mod289-v3 mod289-v4))
+
+
+
+(define-function-library permute-fl ((x :float))
+  (mod289-fl (* (+ (* x 34.0) 1.0) x)))
+
+(define-function-library permute-v3 ((x :vec3))
+  (mod289-v3 (* (+ (* x 34.0) 1.0) x)))
+
+(define-function-library permute-v4 ((x :vec4))
+  (mod289-v4 (* (+ (* x 34.0) 1.0) x)))
+
+(define-macro-library permute (x)
+  (let* ((type (glsl::code-type (glsl::compile-form  x))))
+    (ecase type
+      (:int `(permute-fl (* 1.0 ,x)))
+      (:float `(permute-fl ,x))
+      (:vec3  `(permute-v3 ,x))
+      (:vec4  `(permute-v4 ,x)))))
+
+(unexport '(permute-fl permute-v3 permute-v4))
+
+
+
+(define-function-library taylor-inv-sqrt-fl ((r :float))
+  (- 1.79284291400159d0 (* 0.85373472095314d0 r)))
+
+(define-function-library taylor-inv-sqrt-v4 ((r :vec4))
+  (- 1.79284291400159d0 (* 0.85373472095314d0 r)))
+
+(define-macro-library taylor-inv-sqrt (x)
+  (let* ((type (glsl::code-type (glsl::compile-form  x))))
+    (ecase type
+      (:int `(taylor-inv-sqrt-fl (* 1.0 ,x)))
+      (:float `(taylor-inv-sqrt-fl ,x))
+      (:vec4  `(taylor-inv-sqrt-v4 ,x)))))
+
+(unexport '(taylor-inv-sqrt-fl taylor-inv-sqrt-v4))
+
+
+
+(define-function-library snoise-2d ((v :vec2))
+  (let* ((c (v! 0.211324865405187d0
+		0.366025403784439d0
+		-0.577350269189626d0
+		0.024390243902439d0))
+	 (i (floor (+ v (dot v (yy c)))))
+	 (x0 (+ (- v i) (dot i (xx c))))
+	 (i1 (if (> (x x0) (y x0)) (v! 1.0 0.0) (v! 0.0 1.0)))
+	 (x12 (+ (s~ x0 xyxy) (s~ c xxzz))))
+    (setf (xy x12) (- (xy x12) i1))
+    (setf i (mod289-v2 i))
+    (let* ((p (permute-v3 (+ (permute-v3 (+ (y i) (v! 0.0 (y i1) 1.0))) (x i) (v! 0.0 (x i1) 1.0))))
+	   (m (max (- 0.5 (v! (dot x0 x0) (dot (xy x12) (xy x12)) (dot (s~ x12 zw) (s~ x12 zw)))) 0.0)))
+      (setf m (* m m))
+      (setf m (* m m))
+      (let* ((x (- (* 2.0 (fract (* p (s~ c www)))) 1.0))
+	     (h (- (abs x) 0.5))
+	     (ox (floor (+ x 0.5)))
+	     (a0 (- x ox)))
+	(multf m (- 1.79284291400159d0 (* 0.85373472095314d0 (+ (* a0 a0) (* h h)))))
+	(let* ((g (vec3 0.0)))
+	  (setf (x g) (+ (* (x a0) (x x0)) (* (x h) (y x0))))
+	  (setf (yz g) (+ (* (yz a0) (xz x12)) (* (yz h) (s~ x12 yw))))
+	  (* 130.0 (dot m g)))))))
+
+(define-function-library snoise-3d ((v :vec3))
+  (let* ((c (v! (/ 1.0 6.0) (/ 1.0 3.0)))
+	 (d (v! 0.0 0.5 1.0 2.0))
+	 (i (floor (+ v (dot v (s~ c yyy)))))
+	 (x0 (+ (- v i) (dot i (s~ c xxx))))
+	 (g (step (s~ x0 yzx) (s~ x0 xyz)))
+	 (l (- 1.0 g))
+	 (i1 (min (s~ g xyz) (s~ l zxy)))
+	 (i2 (max (s~ g xyz) (s~ l zxy)))
+	 (x1 (+ (- x0 i1) (s~ c xxx)))
+	 (x2 (+ (- x0 i2) (s~ c yyy)))
+	 (x3 (- x0 (s~ d yyy))))
+    (setf i (mod289-v3 i))
+    (let* ((p (permute-v4 (+ (permute-v4 (+ (permute-v4 (+ (z i) (v! 0.0 (z i1) (z i2) 1.0)))
+				      (y i) (v! 0.0 (y i1) (y i2) 1.0)))
+			  (x i) (v! 0.0 (x i1) (x i2) 1.0))))
+	   (n_ 0.142857142857d0)
+	   (ns (- (* n_ (s~ d wyz)) (s~ d xzx)))
+	   (j (- p (* 49.0 (floor (* p (z ns) (z ns))))))
+	   (x_ (floor (* j (z ns))))
+	   (y_ (floor (- j (* 7.0 x_))))
+	   (x (+ (* x_ (x ns)) (s~ ns yyyy)))
+	   (y (+ (* y_ (x ns)) (s~ ns yyyy)))
+	   (h (- 1.0 (abs x) (abs y)))
+	   (b0 (v! (xy x) (xy y)))
+	   (b1 (v! (s~ x zw) (s~ y zw)))
+	   (s0 (+ (* (floor b0) 2.0) 1.0))
+	   (s1 (+ (* (floor b1) 2.0) 1.0))
+	   (sh (- (step h (vec4 0.0))))
+	   (a0 (+ (s~ b0 xzyw) (* (s~ s0 xzyw) (s~ sh xxyy))))
+	   (a1 (+ (s~ b1 xzyw) (* (s~ s1 xzyw) (s~ sh zzww))))
+	   (p0 (v! (xy a0) (x h)))
+	   (p1 (v! (zw a0) (y h)))
+	   (p2 (v! (xy a1) (z h)))
+	   (p3 (v! (zw a1) (w h)))
+	   (norm (taylor-inv-sqrt-v4 (v! (dot p0 p0) (dot p1 p1) (dot p2 p2) (dot p3 p3)))))
+      (multf p0 (x norm))
+      (multf p1 (y norm))
+      (multf p2 (z norm))
+      (multf p3 (w norm))
+      (let* ((m (max (- 0.6 (v! (dot x0 x0) (dot x1 x1) (dot x2 x2) (dot x3 x3))) 0.0)))
+	(setf m (* m m))
+	(* 42.0 (dot (* m m) (v! (dot p0 x0) (dot p1 x1) (dot p2 x2) (dot p3 x3))))))))
+
+
+(define-function-library grad4 ((j :float) (ip :vec4))
+  (let* ((ones (v! 1.0 1.0 1.0 -1.0))
+	 (p (vec4 0.0))
+	 (s (vec4 0.0)))
+    (setf (xyz p) (- (* (floor (* (fract (* (vec3 j) (xyz ip))) 7.0)) (z ip)) 1.0))
+    (setf (w p) (- 1.5 (dot (abs (s~ p xyz)) (s~ ones xyz))))
+    (setf s (v! (less-than p (vec4 0.0))))
+    (setf (xyz p) (+ (xyz p) (* (- (* (xyz s) 2.0) 1.0) (s~ s www))))
+    p))
+
+
+(define-function-library snoise-4d ((v :vec4))
+  (let* ((c (v! 0.138196601125011d0
+		0.276393202250021d0
+		0.414589803375032d0
+		-0.447213595499958d0))
+	 (i (floor (+ v (dot v (vec4 0.309016994374947451d0)))))
+	 (x0 (+ (- v i) (dot i (s~ c xxxx))))
+	 (i0 (vec4 .0))
+	 (is-x (step (s~ x0 yzw) (s~ x0 xxx)))
+	 (is-yz (step (s~ x0 zww) (s~ x0 yyz))))
+    (setf (x i0) (+ (x is-x) (y is-x) (z is-x)))
+    (setf (s~ i0 yzw) (- 1.0 is-x))
+    (incf (y i0) (+ (x is-yz) (y is-yz)))
+    (incf (zw i0) (- 1.0 (xy is-yz)))
+    (incf (z i0) (z is-yz))
+    (incf (w i0) (- 1.0 (z is-yz)))
+    (let* ((i3 (clamp i0 .0 1.0))
+	   (i2 (clamp (- i0 1.0) 0.0 1.0))
+	   (i1 (clamp (- i0 2.0) 0.0 1.0))
+	   (x1 (+ (- x0 i1) (s~ c xxxx)))
+	   (x2 (+ (- x0 i2) (s~ c yyyy)))
+	   (x3 (+ (- x0 i3) (s~ c zzzz)))
+	   (x4 (+ x0 (s~ c wwww))))
+      (setf i (mod289-v4 i))
+      (let* ((j0 (permute-fl (+ (permute-fl (+ (permute-fl (+ (permute-fl (w i)) (z i))) (y i))) (x i))))
+	     (j1 (permute-v4 (+ (permute-v4 (+ (permute-v4 (+ (permute-v4 (+ (w i) (v! (w i1) (w i2) (w i3) 1.0)))
+						     (z i) (v! (z i1) (z i2) (z i3) 1.0)))
+					  (y i) (v! (y i1) (y i2) (y i3) 1.0)))
+			      (x i) (v! (x i1) (x i2) (x i3) 1.0))))
+	     (ip (v! (/ 1.0 294.0)  (/ 1.0 49.0) (/ 1.0 7.0) 0.0))
+	     (p0 (grad4 j0 ip))
+	     (p1 (grad4 (x j1) ip))
+	     (p2 (grad4 (y j1) ip))
+	     (p3 (grad4 (z j1) ip))
+	     (p4 (grad4 (w j1) ip))
+	     (norm (taylor-inv-sqrt-v4 (v! (dot p0 p0) (dot p1 p1) (dot p2 p2) (dot p3 p3)))))
+	(multf p0 (x norm))
+	(multf p1 (y norm))
+	(multf p2 (z norm))
+	(multf p3 (w norm))
+	(multf p4 (taylor-inv-sqrt-fl (dot p4 p4)))
+	(let* ((m0 (max (- 0.6 (v! (dot x0 x0) (dot x1 x1) (dot x2 x2))) 0.0))
+	       (m1 (max (- 0.6 (v! (dot x3 x3) (dot x4 x4))) 0.0)))
+	  (setf m0 (* m0 m0))
+	  (setf m1 (* m1 m1))
+	  (* 49.0
+	     (+ (dot (* m0 m0) (v! (dot p0 x0) (dot p1 x1) (dot p2 x2)))
+		(dot (* m1 m1) (v! (dot p3 x3) (dot p4 x4))))))))))
+
+
+(define-function-library fsnoise ((c :vec2))
+  (fract (* (sin (dot c (v! 12.9898d0 78.233d0))) 43758.5453d0)))
+
+(define-function-library fsnoise-digits ((c :vec2))
+  (fract (* (sin (dot c (v! 0.129898d0 0.78233d0))) 437.585453d0)))
+
+(define-function-library hsv ((h :float) (s :float) (v :float))
+  (let* ((t (v! 1.0 (/ 2.0 3.0) (/ 1.0 3.0) 3.0))
+	 (p (abs (- (* (fract (+ (vec3 h) (xyz t))) 6.0) (vec3 (w t))))))
+    (* v (mix (vec3 (x t)) (clamp (- p (vec3 (x t))) 0.0 1.0) s))))
+
+(define-function-library rotate-2d ((r :float))
+  (m! (cos r) (sin r) (- (sin r)) (cos r)))
+
+
+(define-function-library rotate-3d ((angle :float) (axis :vec3))
+  (let* ((a (normalize axis))
+	 (s (sin angle))
+	 (c (cos angle))
+	 (r (- 1.0 c)))
+    (m! (+ (* (x a) (x a) r) c)
+	(+ (* (y a) (x a) r) (* (z a) s))
+	(- (* (z a) (x a) r) (* (y a) s))
+	(- (* (x a) (y a) r) (* (z a) s))
+	(+ (* (y a) (y a) r) c)
+	(+ (* (z a) (y a) r) (* (x a) s))
+	(+ (* (x a) (z a) r) (* (y a) s))
+	(- (* (y a) (z a) r) (* (x a) s))
+	(+ (* (z a) (z a) r) c))))
+
+
+
+(define-macro-library for-loop ((bindings limit accur) &body body)
+  `(let ,(cdr bindings)
+     (glsl::for ,(car bindings) ,limit ,accur
+		,@body)))
+
+
+(define-macro-library with-twigl (dummy &body body)
+  (let* ((r (intern "R"))
+	 (m (intern "M"))
+	 (fc (intern "FC"))
+	 (o (intern "O")))
+    `(let* ((,r (xy iresolution))
+	    (,m (/ (xy imouse) ,r))
+	    (_fc (xy gl-frag-coord))
+	    (,fc (v! _fc .5 1.0))
+	    (s 0.0)
+	    (t itime)
+	    (,o (vec4 .0)))
+       ,@body
+       (v! (clamp (xyz ,o) 0.0 1.0) 1.0))))
+
+
+
+;; ================================================================================
+
 
 (gfx:clear-pipeline)
 
