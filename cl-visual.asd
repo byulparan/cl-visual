ada73ee09b986da236ddd9085d16c555868825f2
SungminPark <byulparan@gmail.com>
2023-05-02 02:17:59 +0900

    separate cl-glsl from cl-visual. cl-glsl is crossplatform.

--- cl-visual.asd
+++ cl-visual.asd
@@ -1,31 +1,10 @@
 (asdf:defsystem :cl-visual
   :serial t
   :depends-on (#:cl-nextstep
-	       #:cl-glu
-	       #:cl-glut
-	       #:alexandria
-	       #:cl-ppcre
-	       #:sb-cga
-	       #:split-sequence
-	       #:mathkit
+	       #:cl-glsl
 	       #+sbcl :sb-concurrency
 	       #-sbcl #:safe-queue)
   :components ((:file "package")
-	       (:module "glsl"
-		:components ((:file "type")
-			     (:file "compiler")
-			     (:file "variables")
-			     (:file "functions")
-			     (:file "macros")
-			     (:file "shader")
-			     (:file "gfx/interface")
-			     (:file "gfx/shader-environment")
-			     (:file "gfx/gfx")
-			     (:file "gfx/shader-object")
-			     (:file "gfx/fbo")
-			     (:file "gfx/camera")
-			     (:file "gfx/read-wavefront")
-			     (:file "gfx/mesh")))
 	       (:file "ftgl")
 	       (:file "syphon")
 	       (:file "gl-canvas")
--- package.lisp
+++ package.lisp
@@ -52,8 +52,6 @@
 	   #:imouse
 	   #:vfuv))
 
-
-
 (defpackage :ftgl
   (:use #:cl)
   (:export #:create-pixmap-font
@@ -62,8 +60,6 @@
 	   #:set-font-face-size
 	   #:render-font
 	   #:destroy-font))
-
-
 
 (defpackage :syphon
   (:use :cl)
@@ -78,77 +74,6 @@
 	   #:texture-name
 	   #:texture-size
 	   #:stop-client))
-
-
-(defpackage :glsl
-  (:import-from #:ppcre #:regex-replace-all)
-  (:use #:cl #:alexandria #:split-sequence)
-  (:export ;;#:defpipeline
-	   #:v-defmacro
-	   #:discard
-	   #:lisp
-	   #:true
-	   #:false
-	   #:initialize
-	   #:s~
-	   #:multf
-	   #:cond!
-	   #:if!
-	   #:gl-point-size
-	   #:gl-vertex-id
-	   #:gl-instance-id
-	   #:gl-frag-coord
-	   #:gl-point-coord
-	   #:gl-frag-depth
-	   #:gl-depth-range
-	   #:gl-position))
-
-(defpackage :gfx
-  (:use #:cl #:glsl)
-  (:export #:deg-to-rad
-	   #:get-internal-seconds
-	   #:perspective-matrix
-	   #:ortho-matrix
-
-	   #:shader-environment
-	   #:release-environment
-	   
-	   #:make-gpu-stream
-	   #:gpu-stream-set
-	   #:gpu-stream-length
-	   #:gpu-stream-do-each
-	   
-	   #:make-shader-object
-	   #:with-shader
-	   
-	   #:pull-g
-	   #:defun-g
-	   #:defvar-g
-	   #:defstruct-g
-	   #:defmacro-g
-
-	   #:reinit-shader-system
-	   #:defpipeline
-
-	   #:make-fbo
-	   #:reinit-fbo
-	   #:width
-	   #:height
-	   #:output-texture 
-	   #:release-fbo
-	   #:with-fbo
-	   #:*fbo-stack*
-	   
-	   #:camera
-	   #:track-mouse-spin
-	   #:track-mouse-zoom
-	   #:track-mouse-pan
-	   #:reset-camera
-	   #:eval-camera
-	   #:read-obj-file
-
-	   #:load-quad-stream))
-
 
 (defpackage :post-fx
   (:use :cl :glsl)
